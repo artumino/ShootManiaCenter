656e32827cee759452ef30010308d41102362a99
artumino <jacopo.libe@gmail.com>
2013-08-08 11:38:19 +0200

    Added menu to OAuth2Request Activity

--- java/com/artum/shootmaniacenter/utilities/oauth2/OAth2Request.java
+++ java/com/artum/shootmaniacenter/utilities/oauth2/OAth2Request.java
@@ -2,18 +2,28 @@ package com.artum.shootmaniacenter.utilities.oauth2;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.artum.shootmaniacenter.R;
 import com.artum.shootmaniacenter.ShowAccount;
+import com.artum.shootmaniacenter.adapters.menuItemAdapter;
 import com.artum.shootmaniacenter.global.Variables;
+import com.artum.shootmaniacenter.menu.MenuDrawerClass;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -38,9 +48,17 @@ public class OAth2Request extends Activity {
     String api_secret = "app14185";
     String redirect_uri = "http://localhost:8080/ShootmaniaCenter";
 
+    ActionBarDrawerToggle mDrawerToggle;
+    DrawerLayout mDrawerLayout;
+    ListView mDrawerList;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Variables.menu_selected = 2;
+
         setContentView(R.layout.oauth2_request);
+
+
         refresh = getIntent().getBooleanExtra("refresh", false);
 
 
@@ -68,6 +86,7 @@ public class OAth2Request extends Activity {
             alert.setVisibility(View.INVISIBLE);
             new RefreshToken().execute("");
         }
+        setupDrawer();
     }
 
     private void RunOAth2()
@@ -163,5 +182,89 @@ public class OAth2Request extends Activity {
         }
 
     }
+
+    // <editor-fold defaultstate="collapsed" desc="MENU DRAWER CODE">
+
+    private void setupDrawer()      //Insert this code in OnCreate function!
+    {
+
+        String[] mTitles = getResources().getStringArray(R.array.menu_items);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView)findViewById(R.id.navigation_list);
+
+        ViewGroup.LayoutParams params= mDrawerList.getLayoutParams();
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        double inches = Math.sqrt((metrics.widthPixels*metrics.widthPixels) + (metrics.heightPixels*metrics.heightPixels)) / metrics.densityDpi;
+
+        if(inches > 5)
+            params.width= (metrics.widthPixels / 100) * 50;
+        else
+            params.width = metrics.widthPixels;
+        mDrawerList.setLayoutParams(params);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this,
+                mDrawerLayout,
+                R.drawable.ic_drawer,
+                R.string.app_name,
+                R.string.app_name){
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                invalidateOptionsMenu();
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                invalidateOptionsMenu();
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new menuItemAdapter(this, mTitles));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            MenuDrawerClass menuDrawerClass = new MenuDrawerClass();
+            mDrawerLayout.closeDrawers();
+            menuDrawerClass.selectItem(position, OAth2Request.this);
+        }
+    }
+
+    // </editor-fold>
 
 }
